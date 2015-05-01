0c90dd2d62c40c96fdfb0e3a0fc9f2f925707579
Yunzhen Zhang <z3463065@zmail.unsw.edu.au>
2015-05-01 23:41:43 +1000

    maze led update
    Displays a strip that lights up every 8th LED

--- maze_led/maze_led.ino
+++ maze_led/maze_led.ino
@@ -1,3 +1,23 @@
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
 
+#define PIN 6
+
+#define TOTALLEDS 150
+#define SPACING 8
+
+Adafruit_NeoPixel dots = Adafruit_NeoPixel(TOTALLEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+   dots.begin(); 
+}
+
+void loop() {
+  for (int i = 0; i < TOTALLEDS; i++) {
+    if (i % SPACING == 0) {  
+      dots.setPixelColor(i, dots.Color(50,50, 50));
+      dots.show();
+    }
+    delay(50);
+  }
+}
