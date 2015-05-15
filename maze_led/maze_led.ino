f2f4df95f9569116257d3cee698a33e093a284f2
Yunzhen Zhang <z3463065@zmail.unsw.edu.au>
2015-05-15 23:40:35 +1000

    latest changes

--- maze_led/maze_led.ino
+++ maze_led/maze_led.ino
@@ -4,7 +4,7 @@ Library used: Adafruit NeoPixel: https://github.com/adafruit/Adafruit_NeoPixel
 by Yunzhen Zhang
 
 NOTE: Using an Arduino Mega for this, as Uno does not have enough SRAM to support all the NeoPixels
-(Consider each neopixel takes 3 bytes of memory, we are using 675 of them regardless whether they're on or off)
+(Consider each neopixel takes 3 bytes of memory, we are using 600+ of them regardless whether they're on or off)
 
 To do: investigate a bug when I include Serial.begin() in the code and the code refuses to compile...
 
@@ -13,7 +13,7 @@ To do: investigate a bug when I include Serial.begin() in the code and the code 
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
 
-#define TOTALLEDS 75 // that's 2.5m of led strips
+#define TOTALLEDS 66 // that's 2.5m of led strips
 #define SPACING 8
 #define TOTALSTRIPS 9 //for a 9x9 map
 
@@ -55,22 +55,24 @@ void setup() {
 
 void loop() {
   //if given a coordinate
+  /*
   int x, y;
   x = 8;
   y = 1;
   //implement something here to get x/y    
   if ((x != -1)&&(y != -1)) {
-    /*
+    
     Serial.print("X: ");
     Serial.print(x);
     Serial.print("\tY: ");
     Serial.print(y);
     Serial.println();
-   */
+   
     mapxy[y][x] = 0;
     dots[x].setPixelColor(y*SPACING, dots[x].Color(0,0,0));
     dots[x].show();    
   }
+  */
 }
 
 //shows the initital 9 x 9 grid
