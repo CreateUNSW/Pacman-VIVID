af030ab78308ee9eb2b06aeee1474ee3b4440002
Mark Johnson <mark.johnson.ai@gmail.com>
2015-05-17 13:24:25 +1000

    Changed command definitions to reflect broadcaster.
    Game commands are now bitwise definitions.

--- PacmanRobot/PacmanRobot.ino
+++ PacmanRobot/PacmanRobot.ino
@@ -25,14 +25,14 @@
 #define HEADER 0xC8
 
 // Game commands
-#define NOP      0
-#define START    1
-#define STOP     2
-#define HIDE     3
-#define PAUSE    4
-#define RESUME   5 
-#define RUN_MODE 6
-#define MANUAL_OVERRIDE 7
+#define NOP             B0000000
+#define START           B0000001
+#define STOP            B0000010
+#define HIDE            B0000100
+#define PAUSE           B0001000
+#define RESUME          B0010000 
+#define MUSIC_COMMAND   B0100000
+#define MANUAL_OVERRIDE B1000000
 
 #define GAME_SIZE sizeof(game_state_t)
 
@@ -284,7 +284,7 @@ void loop() {
   /*while (1) {
     update_game();
     
-    if (game.command == MANUAL_OVERRIDE) {
+    if (game.command & MANUAL_OVERRIDE == MANUAL_OVERRIDE) {
        Serial.println("Received instruction..."); 
        game.command = NOP;
        Serial.print("Dir: ");
